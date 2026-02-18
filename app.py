e3593752f2a69ecc1d854d4f7f3d37479e3b8898
Elias Perez <eliasperez1131@gmail.com>
2026-02-18 16:18:54 -0600

    Add basic evaluation module

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,3 +6,14 @@ app = Flask(__name__)
 def home():
     return "EIO-SP Plataforma Activa"
 
+@app.route("/evaluacion", methods=["GET", "POST"])
+def evaluacion():
+    if request.method == "POST":
+        p1 = int(request.form.get("p1", 0))
+        p2 = int(request.form.get("p2", 0))
+
+        total = p1 + p2
+
+        return f"Puntaje total: {total}"
+
+    return render_template("test.html")
