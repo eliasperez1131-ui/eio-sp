a9609ffd006bc7740a79e2288d51d05179acdb1d
Elias Perez <eliasperez1131@gmail.com>
2026-02-18 16:41:48 -0600

    Add dynamic evaluation structure v4

--- app.py
+++ app.py
@@ -2,19 +2,39 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# Reactivos EIO-SP versión básica
+preguntas = [
+    {
+        "id": "p1",
+        "texto": "Tomar un objeto pequeño del servicio no es algo grave.",
+        "dimension": "Honestidad"
+    },
+    {
+        "id": "p2",
+        "texto": "Me irrito fácilmente cuando las cosas no salen como planeé.",
+        "dimension": "Estabilidad"
+    }
+]
+
 @app.route("/")
 def home():
-    return "EIO-SP Plataforma Activa v3"
-
+    return "EIO-SP Plataforma Activa v4"
 
 @app.route("/evaluacion", methods=["GET", "POST"])
 def evaluacion():
+
     if request.method == "POST":
-        p1 = int(request.form.get("p1", 0))
-        p2 = int(request.form.get("p2", 0))
+        resultados = {}
 
-        total = p1 + p2
+        for pregunta in preguntas:
+            valor = int(request.form.get(pregunta["id"], 0))
+            dim = pregunta["dimension"]
 
-        return f"Puntaje total: {total}"
+            if dim not in resultados:
+                resultados[dim] = 0
 
-    return render_template("test.html")
+            resultados[dim] += valor
+
+        return render_template("resultado.html", resultados=resultados)
+
+    return render_template("test.html", preguntas=preguntas)
