6695d523b4f376a78991654aa9b6c412de7f4a37
Elias Perez <eliasperez1131@gmail.com>
2026-02-19 09:00:53 -0600

    Fix PostgreSQL URI format

--- app.py
+++ app.py
@@ -5,7 +5,13 @@ import random
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+uri = os.environ.get("DATABASE_URL")
+
+if uri and uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
